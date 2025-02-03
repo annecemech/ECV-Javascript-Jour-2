1d6dc92616df00d4024bec28cd2fe11c39d083f0
Anne-Cé <34253262+annecemech@users.noreply.github.com>
2025-02-03 16:20:36 +0100

    Correction exercices

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Changer le contenu d'un bouton au clic
+
+const button = document.getElementById("main-button");
+
+button.addEventListener("click", (event) => {
+  if (event.target.innerText === "Bravo!") {
+    event.target.innerText = "Cliquez-ici";
+  } else {
+    event.target.innerText = "Bravo!";
+  }
+});
+
+// Rendre une FAQ cliquable
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("active");
+  });
+});
+
+// Changer une image au clic
+
+const images = document.querySelectorAll("img");
+
+images.forEach((img) => {
+  img.addEventListener("click", (event) => {
+    event.currentTarget.src =
+      "https://images.unsplash.com/photo-1655574282159-2881edbb7d2a?q=80&w=3131&auto=format";
+  });
+});
+
 // Searchbar
 
 const cities = [
@@ -43,3 +76,21 @@ const cities = [
   "Abu Dhabi",
   "Austin",
 ];
+
+const searchBar = document.getElementById("search");
+const resultsContainer = document.getElementById("results");
+
+searchBar.addEventListener("keyup", (event) => {
+  resultsContainer.innerHTML = "";
+  const searchText = event.currentTarget.value.toLowerCase(); // Normaliser la recherche (casse insensible)
+
+  const filteredCities = cities.filter(
+    (city) => city.toLowerCase().includes(searchText) // Vérifier si la ville contient le texte recherché
+  );
+
+  if (searchText !== "") {
+    filteredCities.forEach((city) => {
+      resultsContainer.insertAdjacentHTML("beforeend", `<li>${city}</li>`);
+    }); // Afficher les résultats filtrés
+  }
+});
